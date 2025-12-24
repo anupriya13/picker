90209bdc1dd4d4d2327e816bc7daa50df1176cfc
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-12-24 12:47:44 +0530

    resolve comments

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
- * @format
+ * This file is an entry point for example app
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './app.json';
+import App from './example/src/App';
+import {name as appName} from './example/app.json';
 
 AppRegistry.registerComponent(appName, () => App);
